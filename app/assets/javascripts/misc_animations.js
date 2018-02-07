2951f48b8456b010fd66521e54718f279a930b0f
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-08 00:30:08 +1100

    touchups

--- app/assets/javascripts/misc_animations.js
+++ app/assets/javascripts/misc_animations.js
@@ -1,4 +1,5 @@
-$(function() {
+// for some FUCKED reason you have to reload all javascripts on the 'turbolinks.load' function or else they only work after a page refresh
+$(window).on('turbolinks:load', function() {
   $('.opaque').on('mouseover', function(event) {
     console.log(event.target);
     $(event.target).fadeTo(300, 1);
