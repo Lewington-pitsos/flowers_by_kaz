173752765514613c98ccd45c3707711cd73a04c1
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-08 00:55:57 +1100

    fadein working on single service

--- app/assets/javascripts/misc_animations.js
+++ app/assets/javascripts/misc_animations.js
@@ -1,11 +1,24 @@
 // for some FUCKED reason you have to reload all javascripts on the 'turbolinks.load' function or else they only work after a page refresh
 $(window).on('turbolinks:load', function() {
   $('.opaque').on('mouseover', function(event) {
-    console.log(event.target);
     $(event.target).fadeTo(300, 1);
   })
   $('.opaque').on('mouseleave', function(event) {
-    console.log(event.target);
     $(event.target).fadeTo(300, 0);
+  })
+
+  $(window).scroll(function() {
+    var screenTop = $(window).scrollTop()
+    var screenHeight = $(window).height()
+    var screenBottom = screenTop+ screenHeight
+    var elemTop = $('.servicee').offset().top;
+    var elemBottom = elemTop + $('.servicee').height();
+    console.log(screenBottom);
+    console.log(elemBottom);
+    if (screenBottom > elemBottom) {
+      console.log('passed');
+      $('servicee').removeClass('opaque');
+      $('.servicee').addClass('animated fadeInUpBig');
+    }
   })
 })
