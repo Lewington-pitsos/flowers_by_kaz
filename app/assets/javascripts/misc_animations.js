4015a86a3f24a7026de609cf0b5fe8bd61eea047
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-08 20:00:32 +1100

    smoothened serivce fadins

--- app/assets/javascripts/misc_animations.js
+++ app/assets/javascripts/misc_animations.js
@@ -1,5 +1,29 @@
+function fadeInIfVisible(elements) {
+  // takes in a collection of JQuery elements
+  // works out the current height of the visible window's bottom
+  var screenTop = $(window).scrollTop();
+  var screenHeight = $(window).height();
+  var screenBottom = screenTop + screenHeight;
+
+  // checks each of the passed in elements to see whether their top is more than 120 pixels above the window bottom
+  $(elements).each(function() {
+    var elemTop = $(this).offset().top;
+    var elemBottom = elemTop + 120;
+    if (screenBottom > elemBottom) {
+
+      // if so we remove and add some classes, causeing a fade in
+      $(this).removeClass('hidden');
+      $(this).addClass('animated fadeInUp');
+    }
+  });
+}
+
 // for some FUCKED reason you have to reload all javascripts on the 'turbolinks.load' function or else they only work after a page refresh
+
+
 $(window).on('turbolinks:load', function() {
+
+  // fades in (and then out) overlays when moused over
   $('.opaque').on('mouseover', function(event) {
     $(event.target).fadeTo(300, 1);
   })
@@ -7,24 +31,13 @@ $(window).on('turbolinks:load', function() {
     $(event.target).fadeTo(300, 0);
   })
 
-
-  if ($('.service.hidden').length) {
-    var serviceSelector = $('.service.hidden');
+  // if elligable elements exist, on every scroll event we check if certain elements are in view, and fade them in if they are visible
+  var hiddenServiceElements = $('.service.hidden');
+  if (hiddenServiceElements.length) {
+    fadeInIfVisible(hiddenServiceElements);
 
     $(window).scroll(function() {
-      console.log('srcoll');
-      var screenTop = $(window).scrollTop();
-      var screenHeight = $(window).height();
-      var screenBottom = screenTop + screenHeight;
-
-      $(serviceSelector).each(function() {
-        var elemTop = $(this).offset().top;
-        var elemBottom = elemTop + $(this).height();
-        if (screenBottom > elemBottom) {
-          $(this).removeClass('hidden');
-          $(this).addClass('animated fadeInUpBig');
-        }
-      });
+      fadeInIfVisible(hiddenServiceElements);
     });
   }
 })
