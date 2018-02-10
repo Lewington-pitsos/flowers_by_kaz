50ef2a53d14bb6904de2f6b1950a88c43267c7e4
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-10 12:09:28 +1100

    categories forced into lowest empty position

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,10 +1,20 @@
 module CategoriesHelper
   def save_cat_place(cat, place)
-    # recursive function, if the place has been taken it should increment the place of the work with the matching place and keep trying to save it untill we run out of place conflicts, then save the origional place as intended.
-    same_place = Category.where(position: place)
-    if !same_place.empty?
-      save_cat_place(same_place.take, place + 1)
+    # recursive function
+
+    prev_slot = Category.where(position: place - 1)
+    if prev_slot.empty? && place > 1
+      # if the place is not the first possible place and the previous place slot isn't empty, it should try to slot the category in at the previous place
+      if place > 1
+        save_cat_place(cat, place - 1)
+      end
+    else
+      # if the current place has been taken it should increment the place of the work with the matching place and keep trying to save it untill we run out of place conflicts, then save the origional place as intended.
+      same_place = Category.where(position: place)
+      if !same_place.empty?
+        save_cat_place(same_place.take, place + 1)
+      end
+      cat.update_attribute(:position, place)
     end
-    cat.update_attribute(:position, place)
   end
 end
