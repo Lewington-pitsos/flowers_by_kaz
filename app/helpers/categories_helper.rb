72cbfc6dec50682dece0d4a9528961eedf0e5a21
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-10 12:16:19 +1100

    edited works forced to lowest place

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -9,7 +9,7 @@ module CategoriesHelper
         save_cat_place(cat, place - 1)
       end
     else
-      # if the current place has been taken it should increment the place of the work with the matching place and keep trying to save it untill we run out of place conflicts, then save the origional place as intended.
+      # otherwise, if the current place has been taken it should increment the place of the work with the matching place and keep trying to save it untill we run out of place conflicts, then save the origional place as intended.
       same_place = Category.where(position: place)
       if !same_place.empty?
         save_cat_place(same_place.take, place + 1)
--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -1,10 +1,19 @@
 module WorksHelper
   def save_place(cat, work, place)
-  # recursive function, if the place has been taken it should increment the place of the work with the matching place and keep trying to save it untill we run out of place conflicts, then save the origional place as intended.
-  same_place = cat.works.where(position: place)
-  if !same_place.empty?
-    save_place(cat, same_place.take, place + 1)
+    # recurisve function
+    prev_slot = cat.works.where(position: place - 1)
+    if prev_slot.empty? && place > 1
+      # if the place is not the first possible place and the previous place slot isn't empty, it should try to slot the category in at the previous place
+      if place > 1
+        save_place(cat, work, place - 1)
+      end
+    else
+      # otherwise, if the place has been taken it should increment the place of the work with the matching place and keep trying to save it untill we run out of place conflicts, then save the origional place as intended.
+      same_place = cat.works.where(position: place)
+      if !same_place.empty?
+        save_place(cat, same_place.take, place + 1)
+      end
+      work.update_attribute(:position, place)
+    end
   end
-  work.update_attribute(:position, place)
-end
 end
