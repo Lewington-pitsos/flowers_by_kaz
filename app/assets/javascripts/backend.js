e9f31fb1250bc633af19ab845c796cf7473c18fa
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-11 20:00:00 +1100

    upload spinner for categories

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -1,11 +1,14 @@
 $(window).on('turbolinks:load', function() {
   // when a new file is selected the save option for the edit and new feilds is disabled untill the change event fires (which happens when a file actually gets uploaded)
-  $('#file-upload').on('change', function() {
+  $('.file-upload').on('change', function() {
+    console.log('woot');
     $('#upload-helper').addClass('hidden');
+    $('.loader-wrapper').addClass('hidden');
   })
 
-  $('#file-upload').on('click touchstart' , function(){
+  $('.file-upload').on('click touchstart' , function(){
     $('#upload-helper').removeClass('hidden');
+    $('.loader-wrapper').removeClass('hidden');
   });
 
 })
