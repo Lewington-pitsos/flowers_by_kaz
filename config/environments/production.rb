3b64bd74f05b5bbc1888add29aa4d473b50d146f
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-12 16:38:53 +1100

    secret key generated for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # gets rails production environment to generate a secret key for itself
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
