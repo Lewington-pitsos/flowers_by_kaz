4f24c944ec7b8c0039e9bfbaf3eb503875859f21
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-03-07 20:29:28 +1100

    extraction and commentry

--- app/assets/javascripts/image_loading.js
+++ app/assets/javascripts/image_loading.js
@@ -1,16 +1,23 @@
 function fadeInImage($element, url) {
+  // takes an element and an image url
+  // sets the element's background to that url and fades the element in
   $element.css( "background-image", "url('" + url + "')" ).fadeTo(800, 1);
+}
+
+function loadIndirectly($element) {
+  // expects to be passed a jquery DOM element with a url data attribute that points to an image
+  // creates a new image object and tells it to load that url
+  // when the url is loaded, the background of $element is changed to that same image and the whole affair is faded in
+  var url = $element.data('url');
+  var image = new Image();
+  image.src = url;
+  $(image).on('load', fadeInImage.bind(this, $element, url))
 }
 
 
 $(window).on('turbolinks:load', function() {
-  console.log('image loading');
-
+  // loades all the square work images on the page in indirectly so it looks smooth
   $('.square-img-wrapper').each(function() {
-    var url = $( this ).data('url');
-    $element = $( this );
-    var image = new Image();
-    image.src = url;
-    $(image).on('load', fadeInImage.bind(this, $element, url))
+    loadIndirectly($(this));
   })
 })
